cc5d2c65045353d7f97b5564635f76a0c8e0a00e
Lai Minh Ha <lai.minh@aeris.net>
2022-03-24 17:23:28 +0700

    check out component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { commerce } from './lib/commerce';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -17,8 +17,23 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quanitily) => {
-        const item = await commerce.cart.add(productId, quanitily);
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quanitily);
+        setCart(cart);
+    }
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+        setCart(cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+        setCart(cart);
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+        setCart(cart);
     }
 
     useEffect(() => {
@@ -32,7 +47,21 @@ const App = () => {
                 <Navbar totalItems={cart.total_items} />
                 <Routes>
                     <Route exact path="/" element={<Products products={products} onAddToCart={handleAddToCart} />} />
-                    <Route exact path="/cart" element={<Cart cart={cart} />} />
+                    <Route
+                        exact path="/cart"
+                        element={
+                            <Cart
+                                cart={cart}
+                                handleEmptyCart={handleEmptyCart}
+                                handleRemoveFromCart={handleRemoveFromCart}
+                                handleUpdateCartQty={handleUpdateCartQty}
+                            />}
+                    />
+                    <Route
+                        exact path="/checkout"
+                        element={
+                            <Checkout />
+                        } />
                 </Routes>
             </div>
         </Router>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,15 +11,23 @@ import {
 import { ShoppingCart } from "@material-ui/icons";
 import logo from "../../assets/commerce.png";
 import useStyles from "./styles";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = ({ totalItems }) => {
   const classes = useStyles();
+  const location = useLocation();
+
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography varriant="h6" className={classes.title} color="inherit">
+          <Typography
+            component={Link}
+            to="/"
+            varriant="h6"
+            className={classes.title}
+            color="inherit"
+          >
             <img
               src={logo}
               alt="Commerce.js"
@@ -29,13 +37,20 @@ const Navbar = ({ totalItems }) => {
             Commerce.js
           </Typography>
           <div className={classes.grow} />
-          <div className={classes.button}>
-            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-          </div>
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </>
